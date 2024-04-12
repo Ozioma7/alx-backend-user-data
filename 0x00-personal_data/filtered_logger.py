5b40b62b38faf0ffa95e62747055665b24ab21f8
Ozioma7 <okonisaozioma@gmail.com>
2024-04-12 20:15:10 +0100

    annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,42 +1,37 @@
 #!/usr/bin/env python3
-""" Regex in replacing occurrences of certain field values """
+""" Use of regex in replacing occurrences of certain field values """
 import re
-from typing import List
+from typing import List, Tuple
 import logging
-import mysql.connector
+import mysql.connector # type: ignore
 import os
 
-
 class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class"""
+    """ Redacting Formatter class """
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR: str = ";"
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: List[str]) -> None:
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
+        self.fields: List[str] = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """ Returns filtered values from log records """
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
 
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "password", "ssn", "phone")
 
-PII_FIELDS = ("name", "email", "password", "ssn", "phone")
-
-
-def get_db() -> mysql.connector.connection.MYSQLConnection:
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Connection to MySQL environment """
-    db_connect = mysql.connector.connect(
+    db_connect: mysql.connector.connection.MySQLConnection = mysql.connector.connect(
         user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
         password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
         host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
         database=os.getenv('PERSONAL_DATA_DB_NAME')
     )
     return db_connect
-
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
@@ -46,43 +41,41 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                          f'{field}={redaction}{separator}', message)
     return message
 
-
 def get_logger() -> logging.Logger:
     """ Returns a logging.Logger object """
-    logger = logging.getLogger("user_data")
+    logger: logging.Logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-    target_handler = logging.StreamHandler()
+    target_handler: logging.Handler = logging.StreamHandler()
     target_handler.setLevel(logging.INFO)
 
-    formatter = RedactingFormatter(list(PII_FIELDS))
-    target_handle.setFormatter(formatter)
+    formatter: RedactingFormatter = RedactingFormatter(list(PII_FIELDS))
+    target_handler.setFormatter(formatter)
 
     logger.addHandler(target_handler)
     return logger
 
-
 def main() -> None:
-    """The function will obtain a database connection using get_db
-    and retrieve all rows in the users table and display each row
-    under a filtered format"""
-    db = get_db()
-    cursor = db.cursor()
+    """ Obtain database connection using get_db
+    retrieve all role in the users table and display
+    each row under a filtered format
+    """
+    db: mysql.connector.connection.MySQLConnection = get_db()
+    cursor: mysql.connector.cursor.MySQLCursor = db.cursor()
     cursor.execute("SELECT * FROM users;")
 
-    headers = [field[0] for field in cursor.description]
-    logger = get_logger()
+    headers: List[str] = [field[0] for field in cursor.description]
+    logger: logging.Logger = get_logger()
 
     for row in cursor:
-        info_answer = ''
+        info_answer: str = ''
         for f, p in zip(row, headers):
             info_answer += f'{p}={(f)}; '
         logger.info(info_answer)
 
     cursor.close()
     db.close()
-
 
 if __name__ == '__main__':
     main()
